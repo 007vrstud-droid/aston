0c36ae903ca2857554b76b9e822bb64c3b92aaed
User <vrstud@gmail.com>
2025-11-19 19:13:40 +0300

    задача5 правки

--- astonService/src/main/java/com/example/service/UserService.java
+++ astonService/src/main/java/com/example/service/UserService.java
@@ -9,16 +9,28 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-
+    /**
+     * Создаёт нового пользователя на основе переданного запроса.
+     */
     UserEvent createUser(UserCreateRequest request);
-
+    /**
+     * Обновляет существующего пользователя на основе переданного запроса.
+     */
     UserEvent updateUser(UserUpdateRequest request);
-
+    /**
+     * Удаляет пользователя по его идентификатору.
+     */
     UserEvent deleteUser(Long id);
-
+    /**
+     * Получает информацию о пользователе по его идентификатору.
+     */
     Optional<UserResponse> getUserById(Long id);
-
+    /**
+     * Получает список всех пользователей.
+     */
     List<UserResponse> getAllUsers();
-
+    /**
+     * Проверяет, существует ли пользователь с указанным email.
+     */
     boolean isEmailExists(String email);
 }
